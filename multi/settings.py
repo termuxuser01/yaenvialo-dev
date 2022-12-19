28751df2fdfd86c5d4409eae11f3d3fde53c46fb
termuxuser01 <termuxuser01@gmail.com>
2022-12-19 13:33:14 -0700

    Added get_env option but left insecure key as default

--- multi/settings.py
+++ multi/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0CustomApp/userguideframe.asp/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*unlxi8=mn1)v8fjr23^mts+!9yk!*cc8=3o%ugbztxhf#3os='
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", 'django-insecure-*unlxi8=mn1)v8fjr23^mts+!9yk!*cc8=3o%ugbztxhf#3os=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
