3278fa41832f8aa552545c45dde7aad86c5f8a27
termuxuser01 <termuxuser01@gmail.com>
2022-12-19 23:29:03 -0700

    modified staticdirs

--- multi/settings.py
+++ multi/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles'
 ]
 
 # Default primary key field type
